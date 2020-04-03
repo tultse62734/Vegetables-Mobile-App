9bb94e88427869ec45f3542ed815a216596cd2a3
letuantu1811 <letuantu18111997@gmail.com>
2020-04-03 08:47:32 +0700

    comit

---
+++ VegetableApp/app/src/main/java/com/example/flowermobile/activities/StoreActivity.java
@@ -0,0 +1,22 @@
+package com.example.flowermobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.flowermobile.R;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+    private void initView(){
+
+    }
+    private void initData(){
+
+    }
+}
---
+++ VegetableApp/app/src/main/java/com/example/flowermobile/adapters/StoreAdapter.java
@@ -0,0 +1,5 @@
+package com.example.flowermobile.adapters;
+
+public class StoreAdapter {
+
+}
